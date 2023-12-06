38b2612b02c77802e868249ca91ad56104f37f53
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2023-12-06 21:46:25 +0100

    4-axis controller

--- lib/app/src/enums/enums.dart
+++ lib/app/src/enums/enums.dart
@@ -3,3 +3,5 @@ enum AppTabState { home, controller, profile, failed }
 enum Status { initial, loading, success, failed }
 
 enum ButtonAxis { x, y, z }
+
+enum ButtonDirection { up, down, left, right, stop }
--- lib/controller/view/widgets/body_controller_walk_widget.dart
+++ lib/controller/view/widgets/body_controller_walk_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:robot_controller/app/src/constants/colors.dart';
+import 'package:robot_controller/app/src/constants/paddings.dart';
+import 'package:robot_controller/app/src/enums/enums.dart';
 import 'package:robot_controller/app/view/widgets/app_font_20.dart';
 
 class BodyControllerWalkWidget extends StatefulWidget {
@@ -12,6 +14,7 @@ class BodyControllerWalkWidget extends StatefulWidget {
 
 class _BodyControllerWalkWidgetState extends State<BodyControllerWalkWidget> {
   bool walkValue = false;
+  ButtonDirection buttonDirection = ButtonDirection.stop;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +41,131 @@ class _BodyControllerWalkWidgetState extends State<BodyControllerWalkWidget> {
               },
             )
           ],
-        )
+        ),
+        const SizedBox(
+          height: AppPaddings.globalPadding,
+        ),
+        if (walkValue)
+          SizedBox(
+            width: 300,
+            height: 300,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  borderRadius: BorderRadius.circular(50),
+                  onTap: () {
+                    setState(() {
+                      buttonDirection = ButtonDirection.left;
+                    });
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: buttonDirection == ButtonDirection.left
+                          ? AppColor.lightBlue
+                          : Colors.grey.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Icon(
+                      Icons.keyboard_arrow_left_rounded,
+                      size: 35,
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () {
+                        setState(() {
+                          buttonDirection = ButtonDirection.up;
+                        });
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: buttonDirection == ButtonDirection.up
+                              ? AppColor.lightBlue
+                              : Colors.grey.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: const Icon(Icons.keyboard_arrow_up_rounded,
+                            size: 35),
+                      ),
+                    ),
+                    InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () {
+                        setState(() {
+                          buttonDirection = ButtonDirection.stop;
+                        });
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: buttonDirection == ButtonDirection.stop
+                              ? AppColor.lightBlue
+                              : Colors.grey.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Image.asset(
+                            'assets/hand.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      borderRadius: BorderRadius.circular(50),
+                      onTap: () {
+                        setState(() {
+                          buttonDirection = ButtonDirection.down;
+                        });
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: buttonDirection == ButtonDirection.down
+                              ? AppColor.lightBlue
+                              : Colors.grey.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: const Icon(Icons.keyboard_arrow_down_rounded,
+                            size: 35),
+                      ),
+                    )
+                  ],
+                ),
+                InkWell(
+                  borderRadius: BorderRadius.circular(50),
+                  onTap: () {
+                    setState(() {
+                      buttonDirection = ButtonDirection.right;
+                    });
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: buttonDirection == ButtonDirection.right
+                          ? AppColor.lightBlue
+                          : Colors.grey.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Icon(Icons.keyboard_arrow_right_rounded,
+                        size: 35),
+                  ),
+                ),
+              ],
+            ),
+          )
       ],
     );
   }
